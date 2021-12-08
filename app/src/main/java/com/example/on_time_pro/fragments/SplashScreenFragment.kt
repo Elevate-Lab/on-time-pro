3b39802df8ba6410c8bc1921e6c6b72ba08ba438
Tanushree-coder <tanushree950444@gmail.com>
2021-12-08 18:31:31 +0530

    added splash screen

---
+++ app/src/main/java/com/example/on_time_pro/fragments/SplashScreenFragment.kt
@@ -0,0 +1,30 @@
+package com.example.on_time_pro.fragments
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.on_time_pro.R
+
+class SplashScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler().postDelayed({
+            Navigation.findNavController(view)
+                .navigate(R.id.action_splashScreenFragment_to_mainFragment)
+        }, 1500) //adding 1.5 sec delay
+    }
+
+}
