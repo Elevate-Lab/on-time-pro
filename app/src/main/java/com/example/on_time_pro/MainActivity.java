a14433f2fc40e9af322576590d715329f6ceb8d9
Saloni Doshi <IIT2020059@iiita.ac.in>
2021-12-11 21:49:16 +0530

    added toolbar

--- app/src/main/java/com/example/on_time_pro/MainActivity.java
+++ app/src/main/java/com/example/on_time_pro/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.on_time_pro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
     NavController navController;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +22,15 @@ public class MainActivity extends AppCompatActivity {
         navController = Navigation.findNavController(this, R.id.fragmentContainerView);
         //NavigationUI.setupActionBarWithNavController(this, navController);
 
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.topmenu, menu);
+
+        return true;
     }
 }
