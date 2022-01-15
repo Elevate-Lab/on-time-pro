f9329ff61cbdda80fe3ec1cf4c1c8d6306dd4d8c
tejma17 <tejasmane1710@gmail.com>
2022-01-15 18:06:02 +0530

    Fixed getCourseWork() API

---
+++ app/src/main/java/com/gdsciiita/ontimepro/classes/DueDate.kt
@@ -0,0 +1,7 @@
+package com.gdsciiita.ontimepro.classes
+
+data class DueDate (
+    var year:Int?,
+    var month:Int?,
+    var day:Int?
+    )
---
+++ app/src/main/java/com/gdsciiita/ontimepro/classes/DueTime.kt
@@ -0,0 +1,6 @@
+package com.gdsciiita.ontimepro.classes
+
+data class DueTime (
+    var hours:Int?,
+    var minutes:Int?
+        )
